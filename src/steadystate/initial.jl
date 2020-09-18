99d942995296eaf233bfcd1f8fe845972dac946d
Boyan Bejanov <bbejanov@bankofcanada.ca>
2020-09-18 18:09:40 -0400

    updated to include shocks in ssdata and to handle vartypes

--- src/steadystate/initial.jl
+++ src/steadystate/initial.jl
@@ -2,39 +2,40 @@
 """
     update_auxvars_ss(point, model)
 
-!!! warning "Internal function"
-    This function is for internal use. Do not call directly.
+This function is for internal use. Do not call directly.
 """
 function update_auxvars_ss(point::Vector{Float64}, model::Model)
     nvars = length(model.variables)
     nshks = length(model.shocks)
     nauxs = length(model.auxvars)
-    
+
+    nall = nvars + nshks + nauxs
+
     if nauxs == 0
         return point
     end
-    
+
     trange = -model.maxlag:model.maxlead
     ntimes = length(trange)
     time0 = 1 + model.maxlag
 
     pt0 = [ones(ntimes) 0.0 .+ trange] * reshape(point, 2, :)
-    pt0 = [pt0[:,1:nvars] zeros(ntimes, nshks)]
+    # pt0 = [pt0[:,1:nvars] zeros(ntimes, nshks)]
     pt0 = ModelBaseEcon.update_auxvars(pt0, model)
 
-    result = Vector{Float64}(undef, 2 * (nvars + nauxs))
+    result = Vector{Float64}(undef, 2 * nall)
     result[1:length(point)] = point
     for i = 1:nauxs
-        result[2 * (nvars + i) - 1] = pt0[time0, nvars + nshks + i]
+        result[2 * (nvars + nshks + i) - 1] = pt0[time0, nvars + nshks + i]
     end
 
     if ! model.flags.ssZeroSlope
         shift = model.options.shift
         pt1 = [ones(ntimes) shift .+ trange] * reshape(point, 2, :)
-        pt1 = [pt1[:,1:nvars] zeros(ntimes, nshks)]
+        # pt1 = [pt1[:,1:nvars] zeros(ntimes, nshks)]
         pt1 = ModelBaseEcon.update_auxvars(pt1, model)
         for i = 1:nauxs
-            result[2 * (nvars + i)] = (pt1[time0, nvars + nshks + i] - pt0[time0, nvars + nshks + i]) / shift
+            result[2 * (nvars + nshks + i)] = (pt1[time0, nvars + nshks + i] - pt0[time0, nvars + nshks + i]) / shift
         end
     end
 
@@ -43,22 +44,31 @@ end
 @assert precompile(update_auxvars_ss, (Vector{Float64}, Model))
 
 
-
 """
     _do_update_auxvars_presolve!(model)
 
 Call `update_auxvars_ss`, then call `presolve_sstate!`.
 
-!!! warning "Internal function"
-    This function is for internal use. Do not call directly.
+This function is for internal use. Do not call directly.
 """
 function _do_update_auxvars_presolve!(model::Model; verbose::Bool)
     ss = model.sstate
+    # set shocks (level and slope) and steady slopes to 0.0 presolved
+    for (i, v) in enumerate(model.allvars)
+        if v isa ModelSymbol && v.type == :shock
+            ss.values[2 * i .+ (-1:0)] .= 0.0
+            ss.mask[2 * i .+ (-1:0)] .= true
+        elseif v isa ModelSymbol && v.type == :steady
+            ss.values[2 * i] = 0.0
+            ss.mask[2 * i] = true
+        end
+    end
+    # if all slopes are zero, make it so
     if model.flags.ssZeroSlope
         ss.values[2:2:end] .= 0.0
         ss.mask[2:2:end] .= true
     end
-    # sometimes update_auxvars_ss might change the the behaviour of presolve_sstate!
+    # sometimes update_auxvars_ss might change the behaviour of presolve_sstate!
     # because it might set the values of aux variable differently and so
     # the presolve would be done on a different line parallel to the presolve-variable coordinate.
     aux_vals = update_auxvars_ss(ss.values, model)
@@ -66,7 +76,7 @@ function _do_update_auxvars_presolve!(model::Model; verbose::Bool)
     old_solved = -1
     solved = sum(ss.mask)
     while old_solved < solved
-        presolve_sstate!(model; verbose = verbose)
+        presolve_sstate!(model; verbose=verbose)
         aux_vals .= update_auxvars_ss(ss.values, model)
         ss.values[ .! ss.mask ] = aux_vals[ .! ss.mask ]
         old_solved = solved
@@ -90,14 +100,15 @@ Set the steady state values to the provided defaults and presolve.
 Standard options (default values are taken from `model.options`)
   * `verbose`
 """
-function clear_sstate!(model::Model; lvl = 0.1, slp = 0.0, verbose = model.options.verbose)
+function clear_sstate!(model::Model; lvl=0.1, slp=0.0, verbose=model.options.verbose)
     ss = model.sstate
-    foo = 2 * length(model.variables)
-    ss.values[1:2:foo] .= lvl  # default initial guess for level
-    ss.values[foo + 1:2:end] .= 0.0
-    ss.values[2:2:end] .= slp  # default initial guess for slope
+    nvars = length(model.variables)
+    nshks = length(model.shocks)
+    fill!(ss.values, 0.0)
+    ss.values[1:2:2nvars] .= lvl  # default initial guess for level
+    ss.values[2:2:2nvars] .= slp  # default initial guess for slope
     ss.mask[:] .= false
-    return _do_update_auxvars_presolve!(model; verbose = verbose)
+    return _do_update_auxvars_presolve!(model; verbose=verbose)
 end
 export clear_sstate!
 @assert precompile(clear_sstate!, (Model,))
@@ -123,18 +134,19 @@ Standard options (default values are taken from `model.options`)
   * `verbose`
 
 """
-function initial_sstate!(model::Model, init::AbstractVector{Float64}; verbose = model.options.verbose)
+function initial_sstate!(model::Model, init::AbstractVector{Float64}; verbose=model.options.verbose)
     ss = model.sstate
     nvars = length(model.variables)
+    nshks = length(model.shocks)
     nauxs = length(model.auxvars)
     ninit = length(init)
-    if ninit ∉ (2 * nvars + 2 * nauxs, 2 * nvars)
-        error("Incorrect dimension if initial guess: $(ninit). Expected $(2 * nvars) or $(2 * nvars + 2 * nauxs)")
+    if ninit ∉ (2nvars, 2nvars + 2nauxs, 2nvars + 2nshks + 2nauxs)
+        error("Incorrect dimension if initial guess: $(ninit). Expected $(2nvars) or $(2nvars + 2nauxs) or $(2nvars + 2nshks + 2nauxs)")
     end
     ss.values[1:ninit] = init
     ss.values[ninit + 1:end] .= 0.0
     ss.mask[:] .= false
-    return _do_update_aux_presolve!(model; verbose = verbose)
+    return _do_update_auxvars_presolve!(model; verbose=verbose)
 end
 export initial_sstate!
 @assert precompile(initial_sstate!, (Model, Vector{Float64}))
