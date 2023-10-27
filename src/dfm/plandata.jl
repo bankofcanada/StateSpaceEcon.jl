36b1267cedde840e25cdbed75586c52f525d188f
Boyan Bejanov <bbejanov@gmail.com>
2023-10-27 18:30:54 -0400

    updated plandata.jl for new dfm implementation in ModelBaseEcon

--- src/dfm/plandata.jl
+++ src/dfm/plandata.jl
@@ -1,42 +1,64 @@
 ##################################################################################
-# This file is part of StateSpaceEcon.jl
+# This file is part of jl
 # BSD 3-Clause License
 # Copyright (c) 2020-2022, Bank of Canada
 # All rights reserved.
 ##################################################################################
 
+import ..Plan
+import ..zerodata
+import ..zeroarray
+import ..zeroworkspace
+import ..steadystatedata
+import ..steadystatearray
+import ..steadystateworkspace
 
-function Plans.Plan(m::DFMModel, r::AbstractUnitRange)
-    if !<:(eltype(r),MIT)
-        r = convert(UnitRange{MIT{Unit}}, r)
-    end
-    range = first(r)-m.maxlag:last(r)+m.maxlead
-    names = m.varshks
+Plan(dfm::DFM, rng) = Plan(dfm.model, rng)
+function Plan(m::DFMModel, rng::AbstractUnitRange)
+    eltype(rng) <: MIT || (rng = convert(UnitRange{MIT{Unit}}, rng))
+    range = first(rng)-lags(m):last(rng)+leads(m)
+    names = varshks(m)
     nn = length(names)
     p = Plan(range, (; (@. Symbol(names) => 1:nn)...), falses(length(range), nn))
     p.exogenous[:, isshock.(names)] .= true
     return p
 end
 
-StateSpaceEcon.zeroarray(m::DFMModel, p::Plan) = zeros(size(p.exogenous))
-function StateSpaceEcon.zerodata(m::DFMModel, p::Plan)
-    return MVTSeries(p.range, m.varshks, zeros)
-end
-StateSpaceEcon.zeroworkspace(m::DFMModel, p::Plan) = Workspace(
-    v => zeros(p.range) for v in m.varshks
-)
+@inline zeroarray(::DFM, p::Plan) = zeros(size(p.exogenous))
+@inline zeroarray(::DFMModel, p::Plan) = zeros(size(p.exogenous))
+@inline zerodata(dfm::DFM, p::Plan) = zerodata(dfm.model, p)
+@inline zerodata(m::DFMModel, p::Plan) = MVTSeries(p.range, varshks(m), zeros)
+@inline zeroworkspace(dfm::DFM, p::Plan) = zeroworkspace(dfm.model, p)
+@inline zeroworkspace(m::DFMModel, p::Plan) = Workspace(v => zeros(p.range) for v in varshks(m))
 
-StateSpaceEcon.steadystatearray(m::DFMModel, p::Plan) = steadystatedata(m, p).values
-function StateSpaceEcon.steadystatedata(m::DFMModel, p::Plan)
+@inline steadystatedata(dfm::DFM, p::Plan; ref=0) = steadystatedata(dfm.model, p::Plan, dfm.params)
+function steadystatedata(m::DFMModel, p::Plan, params::DFMParams; ref=0)
     data = zerodata(m, p)
-    data[:, m.variables] .= reshape(m.mean, 1, :)
+    # all means are 0 except possibly observed variables
+    ss = params.observed.mean
+    for (var, val) in zip(keys(ss), values(ss))
+        fill!(data[var], val)
+    end
     return data
 end
-function StateSpaceEcon.steadystateworkspace(m::DFMModel, p::Plan)
-    ret = zeroworkspace(m, p)
-    for (i, v) in enumerate(observed(m))
-        fill!(ret[v], m.mean[i])
+@inline steadystatearray(dfm::DFM, p::Plan; ref=0) = steadystatearray(dfm.model, p, dfm.params)
+function steadystatearray(m::DFMModel, p::Plan, params::DFMParams; ref=0) 
+    data = zeroarray(m, p)
+    ss = params.observed.mean
+    for (n, mv) = enumerate(varshks(m))
+        v = mv.name
+        hasproperty(ss, v) && fill!(view(data, :, n), getproperty(ss, v))
     end
-    return ret
+    return data
+end
+
+@inline steadystateworkspace(m::DFM, p::Plan; ref=0) = steadystateworkspace(m.model, p, m.params)
+function steadystateworkspace(m::DFMModel, p::Plan, params::DFMParams; ref=0)
+    wks = zeroworkspace(m, p)
+    ss = params.observed.mean
+    for (i, v) in enumerate(observed(m))
+        fill!(wks[v], getproperty(ss, v.name))
+    end
+    return wks
 end
 
