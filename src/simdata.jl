881cdfde838caaf39c025490d4961e75af23ef1c
Boyan Bejanov <bbejanov@gmail.com>
2023-12-28 09:39:28 -0500

    indexing with model variables for all MVTSeries, not just SimData

--- src/simdata.jl
+++ src/simdata.jl
@@ -28,20 +28,20 @@ _getname(::ModelVariable{NAME}) where {NAME} = NAME
 
 const _MVCollection = Union{Vector{ModelVariable},NTuple{N,ModelVariable}} where {N}
 # we should allow indexing with model variables
-Base.getindex(sd::SimData, vars::_MVCollection) = getindex(sd, _getname.(vars))
-Base.getindex(sd::SimData, vars::ModelVariable) = getindex(sd, _getname(vars))
-Base.setindex!(sd::SimData, val, vars::_MVCollection) = setindex!(sd, val, _getname.(vars))
-Base.setindex!(sd::SimData, val, vars::ModelVariable) = setindex!(sd, val, _getname(vars))
+Base.getindex(sd::MVTSeries, vars::_MVCollection) = getindex(sd, _getname.(vars))
+Base.getindex(sd::MVTSeries, vars::ModelVariable) = getindex(sd, _getname(vars))
+Base.setindex!(sd::MVTSeries, val, vars::_MVCollection) = setindex!(sd, val, _getname.(vars))
+Base.setindex!(sd::MVTSeries, val, vars::ModelVariable) = setindex!(sd, val, _getname(vars))
 
-Base.getindex(sd::SimData, rows, vars::_MVCollection) = getindex(sd, rows, _getname.(vars))
-Base.getindex(sd::SimData, rows, vars::ModelVariable) = getindex(sd, rows, _getname(vars))
-Base.setindex!(sd::SimData, val, rows, vars::_MVCollection) = setindex!(sd, val, rows, _getname.(vars))
-Base.setindex!(sd::SimData, val, rows, vars::ModelVariable) = setindex!(sd, val, rows, _getname(vars))
+Base.getindex(sd::MVTSeries, rows, vars::_MVCollection) = getindex(sd, rows, _getname.(vars))
+Base.getindex(sd::MVTSeries, rows, vars::ModelVariable) = getindex(sd, rows, _getname(vars))
+Base.setindex!(sd::MVTSeries, val, rows, vars::_MVCollection) = setindex!(sd, val, rows, _getname.(vars))
+Base.setindex!(sd::MVTSeries, val, rows, vars::ModelVariable) = setindex!(sd, val, rows, _getname(vars))
 
-Base.view(sd::SimData, vars::_MVCollection) = view(sd, :, _getname.(vars))
-Base.view(sd::SimData, vars::ModelVariable) = view(sd, :, _getname(vars))
-Base.view(sd::SimData, rows, vars::_MVCollection) = view(sd, rows, _getname.(vars))
-Base.view(sd::SimData, rows, vars::ModelVariable) = view(sd, rows, _getname(vars))
+Base.view(sd::MVTSeries, vars::_MVCollection) = view(sd, :, _getname.(vars))
+Base.view(sd::MVTSeries, vars::ModelVariable) = view(sd, :, _getname(vars))
+Base.view(sd::MVTSeries, rows, vars::_MVCollection) = view(sd, rows, _getname.(vars))
+Base.view(sd::MVTSeries, rows, vars::ModelVariable) = view(sd, rows, _getname(vars))
 
 #######################################################
 
