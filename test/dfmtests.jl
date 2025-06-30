cb3fb28c046a8e162914d7395faae2901e56b827
Boyan Bejanov <bbejanov@bankofcanada.ca>
2025-06-30 19:00:44 -0400

    add first em test for DFM3

--- test/dfmtests.jl
+++ test/dfmtests.jl
@@ -17,21 +17,34 @@ using TimeSeriesEcon.DataEcon
 
 @using_example DFM1
 @using_example DFM2
+@using_example DFM3
+@using_example DFM3MQ
 
 if !isdefined(@__MODULE__, :dfm_check_steadystatedata)
     function dfm_check_steadystatedata(dfm, data, has_rand_shks)
         @unpack model, params = dfm
-        for v in observed(model)
-            @test all(data[v] .== params.observed.mean[v])
+        @test (size(data,2) == nvarshks(dfm))
+        c = 0
+        for on in keys(model.observed)
+            pp = params[on].mean
+            for var in keys(pp)
+                @test all(data[var] .== pp[var])
+                c = c + 1
+            end
         end
         for v in states(model)
             @test iszero(data[v])
+            c = c + 1
         end
         for v in shocks(model)
             @test iszero(data[v]) != has_rand_shks
+            c = c + 1
         end
+        @test c == size(data,2)
     end
 end
+
+##
 
 @testset "ShocksSampler" begin
     # test constructors of various kinds
@@ -58,145 +71,92 @@ end
 end
 
 
-@testset "DFM1" begin
-    dfm = DFM1.newmodel()
-    @unpack model, params = dfm
-    @test !any(isnan, params) && lags(dfm) == 1
-
-    rng = 1U:300U
-    @test (Plan(dfm, rng); true)
-    pl = Plan(dfm, rng)
-    @test (rangeof(pl) == 0U:300U)
-    vars = [i for (i, v) in enumerate(varshks(dfm)) if v ∉ shocks(dfm)]
-    shks = [i for (i, v) in enumerate(varshks(dfm)) if v ∈ shocks(dfm)]
-    @test all(pl.exogenous[:, shks])
-    @test !any(pl.exogenous[:, vars])
-
-    @test (steadystatedata(dfm, pl) isa MVTSeries)
-    @test (steadystatearray(dfm, pl) isa Matrix)
-    @test (steadystateworkspace(dfm, pl) isa Workspace)
-
-    @test (z = zerodata(dfm, pl); z isa MVTSeries && iszero(z))
-    @test (z = zeroarray(dfm, pl); z isa Matrix && iszero(z))
-    @test (z = zeroworkspace(dfm, pl); z isa Workspace && all(iszero, values(z)))
-
-    data = steadystatedata(dfm, pl)
-    dfm_check_steadystatedata(dfm, data, false)
-    @test (rand_shocks!(dfm, pl, data); true)
-    dfm_check_steadystatedata(dfm, data, true)
-
-    data = steadystatedata(dfm, pl)
-    ss = ShocksSampler(dfm)
-    rand_shocks!(ss, 1U:10U, data)
-    dfm_check_steadystatedata(dfm, data, true)
-    @test iszero(data[11U:end, shocks(dfm)])
-
-    @test (simulate(dfm, pl, data); true)
-    sim = simulate(dfm, pl, data)
-
-    exog_endo!(pl, [:a], [:a_shk], rng[1:5])
-    @test_throws "Non-empty plan" simulate(dfm, pl, data)
-
+if !isdefined(@__MODULE__, :do_test_dfm)
+    function do_test_dfm(MOD, nlags)
+        dfm = MOD.newmodel()
+        @unpack model, params = dfm
+        @test !any(isnan, params) && lags(dfm) == nlags
+    
+        rng = 1U:300U
+        @test (Plan(dfm, rng); true)
+        pl = Plan(dfm, rng)
+        @test (rangeof(pl) == (1-nlags)*U:300U)
+        vars = [i for (i, v) in enumerate(varshks(dfm)) if v ∉ shocks(dfm)]
+        shks = [i for (i, v) in enumerate(varshks(dfm)) if v ∈ shocks(dfm)]
+        @test all(pl.exogenous[:, shks])
+        @test !any(pl.exogenous[:, vars])
+    
+        @test (steadystatedata(dfm, pl) isa MVTSeries)
+        @test (steadystatearray(dfm, pl) isa Matrix)
+        @test (steadystateworkspace(dfm, pl) isa Workspace)
+    
+        @test (z = zerodata(dfm, pl); z isa MVTSeries && iszero(z))
+        @test (z = zeroarray(dfm, pl); z isa Matrix && iszero(z))
+        @test (z = zeroworkspace(dfm, pl); z isa Workspace && all(iszero, values(z)))
+    
+        data = steadystatedata(dfm, pl)
+        dfm_check_steadystatedata(dfm, data, false)
+        @test (rand_shocks!(dfm, pl, data); true)
+        dfm_check_steadystatedata(dfm, data, true)
+    
+        data = steadystatedata(dfm, pl)
+        ss = ShocksSampler(dfm)
+        rand_shocks!(ss, 1U:10U, data)
+        dfm_check_steadystatedata(dfm, data, true)
+        @test iszero(data[11U:end, shocks(dfm)])
+    
+        @test (simulate(dfm, pl, data); true)
+    
+        exog_endo!(pl, [:a], [:a_shk], rng[1:5])
+        @test_throws "Non-empty plan" simulate(dfm, pl, data)
+    
+    end
 end
 
-
-@testset "DFM2" begin
-    dfm = DFM2.newmodel()
-    @unpack model, params = dfm
-    @test !any(isnan, params) && lags(dfm) == 2
-
-    rng = 2U:300U
-    @test (Plan(dfm, rng); true)
-    pl = Plan(dfm, rng)
-    @test (rangeof(pl) == 0U:300U)
-    vars = [i for (i, v) in enumerate(varshks(dfm)) if v ∉ shocks(dfm)]
-    shks = [i for (i, v) in enumerate(varshks(dfm)) if v ∈ shocks(dfm)]
-    @test all(pl.exogenous[:, shks])
-    @test !any(pl.exogenous[:, vars])
-
-    @test (steadystatedata(dfm, pl) isa MVTSeries)
-    @test (steadystatearray(dfm, pl) isa Matrix)
-    @test (steadystateworkspace(dfm, pl) isa Workspace)
-
-    @test (z = zerodata(dfm, pl); z isa MVTSeries && iszero(z))
-    @test (z = zeroarray(dfm, pl); z isa Matrix && iszero(z))
-    @test (z = zeroworkspace(dfm, pl); z isa Workspace && all(iszero, values(z)))
-
-    data = steadystatedata(dfm, pl)
-    dfm_check_steadystatedata(dfm, data, false)
-    @test (rand_shocks!(dfm, pl, data); true)
-    dfm_check_steadystatedata(dfm, data, true)
-
-    data = steadystatedata(dfm, pl)
-    ss = ShocksSampler(dfm)
-    rand_shocks!(ss, 2U:10U, data)
-    dfm_check_steadystatedata(dfm, data, true)
-    @test iszero(data[11U:end, shocks(dfm)])
-
-    @test (simulate(dfm, pl, data); true)
-    sim = simulate(dfm, pl, data)
-
-    exog_endo!(pl, [:a], [:a_shk], rng[1:5])
-    @test_throws "Non-empty plan" simulate(dfm, pl, data)
-
+@testset "DFM" begin
+    do_test_dfm(DFM1, 1)
+    do_test_dfm(DFM2, 2)
+    do_test_dfm(DFM3, 2)
+    do_test_dfm(DFM3MQ, 5)
 end
 
+##
 
 td = DataEcon.readdb(joinpath(@__DIR__, "data", "dfm.daec"))
-@testset "DFM1Filter" begin
-    dfm = DFM1.newmodel()
-    rng = rangeof(td.dfm1.shks; drop=lags(dfm))
-    p = Plan(dfm, rng)
-    data = steadystatedata(dfm, p)
-    data[rng, :] .= 0
-    data[rng, :] = td.dfm1.shks
-    sim = simulate(dfm, p, data)
-    @test @compare sim td.dfm1.sim quiet
-
-    Y = sim[rng, observed(dfm)].values
-    x0 = sim[begin, states_with_lags(dfm)]
-    Px0 = I(nstates_with_lags(dfm))
-    kfd = kf_filter(Y, x0, Px0, dfm)
-    @test @compare kfd2data(kfd, :update, dfm, rng) td.dfm1.update quiet
-    @test @compare kfd2data(kfd, :pred, dfm, rng) td.dfm1.pred quiet
-    kfd = kf_smoother(Y, x0, Px0, dfm)
-    @test @compare kfd2data(kfd, :smooth, dfm, rng) td.dfm1.smooth quiet
-
-    for which in (:update, :pred, :smooth)
-        a1 = kfd2data(kfd, which, dfm, rng; states_with_lags=false)
-        a2 = kfd2data(kfd, which, dfm, rng; states_with_lags=true)
-        @test @compare(a1, a2, quiet) == (lags(dfm) == 1)
-        @test @compare(a1, a2, quiet, ignoremissing) == true
+if !isdefined(@__MODULE__, :do_test_filter)
+    function do_test_filter(MOD, TD)
+        dfm = MOD.newmodel()
+        rng = rangeof(TD.shks; drop=lags(dfm))
+        p = Plan(dfm, rng)
+        data = steadystatedata(dfm, p)
+        data[rng, :] .= 0
+        data[rng, :] = TD.shks
+        sim = simulate(dfm, p, data)
+        @test @compare sim TD.sim quiet
+    
+        Y = sim[rng, observed(dfm)].values
+        x0 = zeros(nstates_with_lags(dfm))
+        Px0 = I(nstates_with_lags(dfm))
+        kfd = kf_filter(Y, x0, Px0, dfm)
+        @test @compare kfd2data(kfd, :update, dfm, rng) TD.update quiet
+        @test @compare kfd2data(kfd, :pred, dfm, rng) TD.pred quiet
+        kfd = kf_smoother(Y, x0, Px0, dfm)
+        @test @compare kfd2data(kfd, :smooth, dfm, rng) TD.smooth quiet
+    
+        for which in (:update, :pred, :smooth)
+            a1 = kfd2data(kfd, which, dfm, rng; states_with_lags=false)
+            a2 = kfd2data(kfd, which, dfm, rng; states_with_lags=true)
+            @test @compare(a1, a2, quiet) == (lags(dfm) == 1)
+            @test @compare(a1, a2, quiet, ignoremissing) == true
+        end
     end
 end
 
-## 
-
-@testset "DFM2Filter" begin
-    dfm = DFM2.newmodel()
-    rng = rangeof(td.dfm2.shks; drop=lags(dfm))
-    p = Plan(dfm, rng)
-    data = steadystatedata(dfm, p)
-    data[rng, :] .= 0
-    data[rng, :] = td.dfm2.shks
-    sim = simulate(dfm, p, data)
-    @test @compare sim td.dfm2.sim quiet
-
-    Y = sim[rng, observed(dfm)].values
-    x0 = zeros(nstates_with_lags(dfm))
-    Px0 = I(nstates_with_lags(dfm))
-    kfd = kf_filter(Y, x0, Px0, dfm)
-    @test @compare kfd2data(kfd, :update, dfm, rng) td.dfm2.update quiet
-    @test @compare kfd2data(kfd, :pred, dfm, rng) td.dfm2.pred quiet
-    kfd = kf_smoother(Y, x0, Px0, dfm)
-    @test @compare kfd2data(kfd, :smooth, dfm, rng) td.dfm2.smooth quiet
-
-    for which in (:update, :pred, :smooth)
-        a1 = kfd2data(kfd, which, dfm, rng; states_with_lags=false)
-        a2 = kfd2data(kfd, which, dfm, rng; states_with_lags=true)
-        @test @compare(a1, a2, quiet) == (lags(dfm) == 1)
-        @test @compare(a1, a2, quiet, ignoremissing) == true
-    end
+@testset "DFM Filter" begin
+    do_test_filter(DFM1, td.dfm1)
+    do_test_filter(DFM2, td.dfm2)
+    do_test_filter(DFM3, td.dfm3)
+    do_test_filter(DFM3MQ, td.dfm3mq)
 end
 
 ##
@@ -378,3 +338,15 @@ end
 
 end
 
+## 
+
+@testset "DFM3 EM" begin
+    m = DFM3.newmodel()
+    true_p = copy(m.params)
+    Y = td.dfm3.sim[1U:end, observed(m)].values
+
+    fill!(m.params, NaN)
+    DFMSolver.EMestimate!(m, Y, rftol=1e-5, verbose=false)
+    @test compare(m.params, td.dfm3.em_p1, quiet=true)
+    
+end
